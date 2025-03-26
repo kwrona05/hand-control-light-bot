5d9ade4e8cbe2fcd3830f25727fd659a70dfd82b
kwrona05 <kwrona05@gmail.com>
2025-03-26 21:44:15 +0100

    Add more configurations

--- controller.py
+++ controller.py
@@ -47,4 +47,45 @@ def leds_control(fingerUp):
         led_3.write(1)
         led_4.write(1)
         led_5.write(1)
-    
+    elif fingerUp == [1, 0, 0, 0, 0]:
+        led_1.write(1)
+        led_2.write(0)
+        led_3.write(0)
+        led_4.write(0)
+        led_5.write(0)
+    elif fingerUp == [1, 1, 0, 0, 0]:
+        led_1.write(1)
+        led_2.write(1)
+        led_3.write(0)
+        led_4.write(0)
+        led_5.write(0)
+    elif fingerUp == [1, 1, 1, 0, 0]:
+        led_1.write(1)
+        led_2.write(1)
+        led_3.write(1)
+        led_4.write(0)
+        led_5.write(0)
+    elif fingerUp == [1, 1, 1, 1, 0]:
+        led_1.write(1)
+        led_2.write(1)
+        led_3.write(1)
+        led_4.write(1)
+        led_5.write(0)
+    elif fingerUp == [0, 0, 0, 0, 1]:
+        led_1.write(0)
+        led_2.write(0)
+        led_3.write(0)
+        led_4.write(0)
+        led_5.write(1)
+    elif fingerUp == [0, 0, 0, 1, 1]:
+        led_1.write(0)
+        led_2.write(0)
+        led_3.write(0)
+        led_4.write(1)
+        led_5.write(1)
+    elif fingerUp == [0, 0, 1, 1, 1]:
+        led_1.write(0)
+        led_2.write(0)
+        led_3.write(1)
+        led_4.write(1)
+        led_5.write(1)
